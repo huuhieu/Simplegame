b576359450af12bcf1cede58e4f4baa77baea6a5
HuuHieu <hieudh55@gmail.com>
2013-07-24 11:08:26 +0700

    Initial Commit

---
+++ Cocos2dSimpleGame/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  Cocos2dSimpleGameAppController.h
+//  Cocos2dSimpleGame
+//
+//  Created by MAC on 7/24/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ Cocos2dSimpleGame/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  Cocos2dSimpleGameAppController.h
+//  Cocos2dSimpleGame
+//
+//  Created by MAC on 7/24/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
